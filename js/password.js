50e50f70f05daf997a336e9a8b0d987872d526c5
nmuffet <91439683+nmuffet@users.noreply.github.com>
2023-01-12 19:12:24 -0500

    mortar added

---
+++ js/password.js
@@ -0,0 +1,12 @@
+var password;
+
+var pass1="cool";
+
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="/studentwork.html";
+    }
